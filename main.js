7a79242bd1cdebbafb9c50dacce51d2e3ed1f75c
AliiDak <dakoumi31@gmail.com>
2022-03-22 14:56:24 +0100

    t commit

---
+++ main.js
@@ -0,0 +1,278 @@
+gsap.registerPlugin(Flip);
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
+const services = document.querySelector(".services");
+const title = document.querySelector("#title");
+const hometitle = document.querySelector(".hometitle");
+const infos = document.querySelectorAll(".info");
+const img = document.querySelector(".item > img");
+
+const state = Flip.getState(about);
+const state1 = Flip.getState(contact);
+const state2 = Flip.getState(services);
+
+var textWrapper = document.querySelector(".ml6 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+const ml6 = document.querySelectorAll(".ml6 .letter");
+
+gsap.fromTo(
+  ml6,
+  0.5,
+  {
+    y: "1.5em",
+  },
+  {
+    y: "0",
+    stagger: 0.1,
+    ease: "back.out(1.7)",
+  }
+);
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("[data-scroll-container]"),
+//   smooth: true,
+// });
+const autofunctionA = (container) => {
+  const about = document.querySelector(".about");
+  about.classList.add("bigimage");
+  const tl = gsap
+    .timeline()
+    .to(hometitle, 0.5, { opacity: 0 })
+    .to(infos, 0.5, {
+      opacity: 0,
+      onComplete: () => {
+        Flip.fit(about, ".fullscreen", {
+          duration: 1,
+          ease: Expo.easeInOut,
+          absolute: true,
+        });
+      },
+    });
+  return tl;
+};
+const functionA = (container) => {
+  const about = document.querySelector(".about");
+  about.classList.add("bigimage");
+
+  return Flip.fit(about, ".fullscreen", {
+    duration: 1,
+    ease: Expo.easeInOut,
+    absolute: true,
+    clearProps: "all",
+  });
+};
+const functionC = (container) => {
+  const contact = document.querySelector(".contact");
+  contact.classList.add("bigimage");
+  return Flip.fit(contact, ".fullscreen", {
+    duration: 1,
+    ease: Expo.easeInOut,
+  });
+};
+const functionS = (container) => {
+  const services = document.querySelector(".services");
+  services.classList.add("bigimage");
+  return Flip.fit(services, ".fullscreen", {
+    duration: 1,
+    ease: Expo.easeInOut,
+  });
+};
+const enterAnimation = (container, time) => {
+  const title = document.querySelector("#title");
+  const tl = gsap.timeline();
+  tl.from("#title", 0.7, { opacity: 0, y: 150, ease: Expo.easeInOut });
+  return tl;
+};
+const enterAnimationH = (container, time) => {
+  var textWrapper = document.querySelector(".ml6 .letters");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+  const ml6 = document.querySelectorAll(".ml6 .letter");
+  const tl = gsap.timeline();
+
+  tl.from(container, 0.5, { opacity: 0 });
+  tl.fromTo(
+    ml6,
+    0.5,
+    {
+      y: "1.5em",
+    },
+    {
+      y: "0",
+      stagger: 0.1,
+      ease: "back.out(1.7)",
+    },
+    "-=0.3"
+  );
+  return tl;
+};
+const leaveAnimation = (container) => {
+  return gsap.to(container, 0.3, {
+    opacity: 0,
+  });
+};
+const leaveAnimationAbout = (container) => {
+  return gsap.to(".about", 0.3, {
+    opacity: 0,
+  });
+};
+
+const select = (e) => document.querySelector(e);
+let bodyScrollBar;
+
+function initSmoothScrollbar() {
+  bodyScrollBar = Scrollbar.init(select("#viewport"), { damping: 0.04 });
+  // remove horizontal scrollbar
+  bodyScrollBar.track.xAxis.element.remove();
+}
+barba.hooks.after(() => {
+  const ml6 = document.querySelectorAll(".ml6 .letter");
+  bodyScrollBar.update();
+  bodyScrollBar.scrollTo(0, 0);
+  img.addEventListener("click", () => {
+    gsap.to(hometitle, 0, { opacity: 0 });
+    gsap.to(infos, 0, {
+      opacity: 0,
+    });
+  });
+});
+// Smoooth Scrollbar
+
+// const asscroll = new ASScroll({
+//   disableRaf: true,
+// });
+// window.addEventListener("load", () => {});
+// barba.hooks.after(() => {
+//   asscroll.update();
+//   asscroll.scrollTo(0, 0);
+// });
+barba.init({
+  views: [
+    {
+      namespace: "about",
+      beforeEnter(data) {
+        window.scrollTo(0, 0);
+        const title = document.querySelector(".imagecontainer .pagetitle");
+        const hometitle = document.querySelector(".hometitle");
+        const infos = document.querySelectorAll(".info");
+        const img = document.querySelector(".item > img");
+      },
+    },
+    {
+      namespace: "contact",
+      beforeEnter(data) {
+        scrolltop();
+        gsap.ticker.add(asscroll.update);
+        window.onbeforeunload = function () {
+          window.scrollTo(0, 0);
+        };
+      },
+    },
+    {
+      namespace: "home",
+      afterEnter(data) {},
+    },
+  ],
+  transitions: [
+    {
+      name: "from-home-transition",
+      to: {
+        namespace: ["about"],
+      },
+      async leave({ current }) {
+        const hometitle = document.querySelector(".hometitle");
+        const infos = document.querySelectorAll(".info");
+        const img = document.querySelector(".item > img");
+        const tl = gsap.timeline();
+        await tl.to(hometitle, 0.5, { opacity: 0 }).to(
+          infos,
+          0.5,
+          {
+            opacity: 0,
+          },
+          "-=0.5"
+        );
+        await functionA(current.container);
+      },
+
+      enter({ next }) {
+        enterAnimation(next.container, 0);
+      },
+      once({ next }) {
+        initSmoothScrollbar();
+      },
+    },
+    {
+      name: "from-home-transition",
+      to: {
+        namespace: ["contact"],
+      },
+      async leave({ current }) {
+        const hometitle = document.querySelector(".hometitle");
+        const infos = document.querySelectorAll(".info");
+        const img = document.querySelector(".item > img");
+        const tl = gsap.timeline();
+        await tl.to(hometitle, 0.5, { opacity: 0 }).to(
+          infos,
+          0.5,
+          {
+            opacity: 0,
+          },
+          "-=0.5"
+        );
+        await functionC(current.container);
+      },
+      once: ({ next }) => {
+        initSmoothScrollbar();
+      },
+      enter({ next }) {
+        enterAnimation(next.container, 0);
+      },
+    },
+    {
+      name: "from-home-transition",
+      to: {
+        namespace: ["services"],
+      },
+      async leave({ current }) {
+        const hometitle = document.querySelector(".hometitle");
+        const infos = document.querySelectorAll(".info");
+        const img = document.querySelector(".item > img");
+        const tl = gsap.timeline();
+        await tl.to(hometitle, 0.5, { opacity: 0 }).to(
+          infos,
+          0.5,
+          {
+            opacity: 0,
+          },
+          "-=0.5"
+        );
+        await functionS(current.container);
+      },
+      once: ({ next }) => {
+        initSmoothScrollbar();
+      },
+      enter({ next }) {
+        enterAnimation(next.container, 0);
+      },
+    },
+    {
+      name: "to-home-transition",
+      to: {
+        namespace: ["home"],
+      },
+      once: ({ next }) => {
+        initSmoothScrollbar();
+      },
+      leave: ({ current }) => leaveAnimationAbout(current.container),
+
+      enter({ next }) {
+        enterAnimationH(next.container, 1);
+      },
+    },
+  ],
+});
---
+++ test.js
@@ -0,0 +1,328 @@
+import _ as THREE from "three";
+import ASScroll from "@ashthornton/asscroll";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import fragment from "./shaders/fragment.glsl";
+import vertex from "./shaders/vertex.glsl";
+// import testTexture from '../img/texture.jpg';
+import _ as dat from "dat.gui";
+import gsap from "gsap";
+import barba from "@barba/core";
+
+export default class Sketch {
+constructor(options) {
+this.container = options.domElement;
+this.width = this.container.offsetWidth;
+this.height = this.container.offsetHeight;
+
+    this.camera = new THREE.PerspectiveCamera(
+      30,
+      this.width / this.height,
+      10,
+      1000
+    );
+    this.camera.position.z = 600;
+
+    this.camera.fov = (2 * Math.atan(this.height / 2 / 600) * 180) / Math.PI;
+    this.imagesAdded = 0;
+
+    this.scene = new THREE.Scene();
+
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    // this.renderer.setPixelRatio(2);
+    this.container.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.materials = [];
+
+    this.asscroll = new ASScroll({
+      disableRaf: true,
+    });
+
+    this.asscroll.enable({
+      horizontalScroll: !document.body.classList.contains("b-inside"),
+    });
+    this.time = 0;
+    // this.setupSettings()
+
+    this.addObjects();
+    this.addClickEvents();
+    this.resize();
+    this.render();
+
+    this.barba();
+
+    this.setupResize();
+
+}
+
+barba() {
+this.animationRunning = false;
+let that = this;
+barba.init({
+transitions: [
+{
+name: "from-home-transition",
+from: {
+namespace: ["home"],
+},
+leave(data) {
+that.animationRunning = true;
+that.asscroll.disable();
+return gsap.timeline().to(data.current.container, {
+opacity: 0,
+delay: 7,
+});
+},
+enter(data) {
+that.asscroll = new ASScroll({
+disableRaf: true,
+containerElement: data.next.container.querySelector(
+"[asscroll-container]"
+),
+});
+that.asscroll.enable({
+newScrollElements:
+data.next.container.querySelector(".scroll-wrap"),
+});
+return gsap.timeline().from(data.next.container, {
+opacity: 0,
+onComplete: () => {
+that.container.style.visibility = "hidden";
+that.animationRunning = false;
+},
+});
+},
+},
+{
+name: "from-inside-page-transition",
+from: {
+namespace: ["inside"],
+},
+leave(data) {
+that.asscroll.disable();
+return gsap
+.timeline()
+.to(".curtain", {
+duration: 0.3,
+y: 0,
+})
+.to(data.current.container, {
+opacity: 0,
+});
+},
+enter(data) {
+that.asscroll = new ASScroll({
+disableRaf: true,
+containerElement: data.next.container.querySelector(
+"[asscroll-container]"
+),
+});
+that.asscroll.enable({
+horizontalScroll: true,
+newScrollElements:
+data.next.container.querySelector(".scroll-wrap"),
+});
+// cleeaning old arrays
+that.imageStore.forEach((m) => {
+that.scene.remove(m.mesh);
+});
+that.imageStore = [];
+that.materials = [];
+that.addObjects();
+that.resize();
+that.addClickEvents();
+that.container.style.visibility = "visible";
+
+            return gsap
+              .timeline()
+              .to(".curtain", {
+                duration: 0.3,
+                y: "-100%",
+              })
+              .from(data.next.container, {
+                opacity: 0,
+              });
+          },
+        },
+      ],
+    });
+
+}
+
+addClickEvents() {
+this.imageStore.forEach((i) => {
+i.img.addEventListener("click", () => {
+const tl = gsap
+.timeline()
+.to(i.mesh.material.uniforms.uCorners.value, {
+x: 1,
+duration: 3,
+})
+.to(
+i.mesh.material.uniforms.uCorners.value,
+{
+y: 1,
+duration: 6,
+},
+0.1
+)
+.to(
+i.mesh.material.uniforms.uCorners.value,
+{
+z: 1,
+duration: 6,
+},
+0.2
+)
+.to(
+i.mesh.material.uniforms.uCorners.value,
+{
+w: 1,
+duration: 6,
+},
+0.3
+);
+});
+});
+}
+setupSettings() {
+this.settings = {
+progress: 0,
+};
+this.gui = new dat.GUI();
+this.gui.add(this.settings, "progress", 0, 1, 0.001);
+}
+
+resize() {
+this.width = this.container.offsetWidth;
+this.height = this.container.offsetHeight;
+this.renderer.setSize(this.width, this.height);
+this.camera.aspect = this.width / this.height;
+this.camera.updateProjectionMatrix();
+
+    this.camera.fov = (2 * Math.atan(this.height / 2 / 600) * 180) / Math.PI;
+
+    this.materials.forEach((m) => {
+      m.uniforms.uResolution.value.x = this.width;
+      m.uniforms.uResolution.value.y = this.height;
+    });
+
+    this.imageStore.forEach((i) => {
+      let bounds = i.img.getBoundingClientRect();
+      i.mesh.scale.set(bounds.width, bounds.height, 1);
+      i.top = bounds.top;
+      i.left = bounds.left + this.asscroll.currentPos;
+      i.width = bounds.width;
+      i.height = bounds.height;
+
+      i.mesh.material.uniforms.uQuadSize.value.x = bounds.width;
+      i.mesh.material.uniforms.uQuadSize.value.y = bounds.height;
+
+      i.mesh.material.uniforms.uTextureSize.value.x = bounds.width;
+      i.mesh.material.uniforms.uTextureSize.value.y = bounds.height;
+    });
+
+}
+
+setupResize() {
+window.addEventListener("resize", this.resize.bind(this));
+}
+
+addObjects() {
+this.geometry = new THREE.PlaneBufferGeometry(1, 1, 100, 100);
+console.log(this.geometry);
+this.material = new THREE.ShaderMaterial({
+// wireframe: true,
+uniforms: {
+time: { value: 1.0 },
+uProgress: { value: 0 },
+uTexture: { value: null },
+uTextureSize: { value: new THREE.Vector2(100, 100) },
+uCorners: { value: new THREE.Vector4(0, 0, 0, 0) },
+uResolution: { value: new THREE.Vector2(this.width, this.height) },
+uQuadSize: { value: new THREE.Vector2(300, 300) },
+},
+vertexShader: vertex,
+fragmentShader: fragment,
+});
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.scale.set(300, 300, 1);
+    // this.scene.add( this.mesh );
+    this.mesh.position.x = 300;
+
+    this.images = [...document.querySelectorAll(".js-image")];
+
+    this.imageStore = this.images.map((img) => {
+      let bounds = img.getBoundingClientRect();
+      let m = this.material.clone();
+      this.materials.push(m);
+      let texture = new THREE.Texture(img);
+      texture.needsUpdate = true;
+
+      m.uniforms.uTexture.value = texture;
+
+      // img.addEventListener('mouseout',()=>{
+      //     this.tl = gsap.timeline()
+      //     .to(m.uniforms.uCorners.value,{
+      //         x:0,
+      //         duration: 0.4
+      //     })
+      //     .to(m.uniforms.uCorners.value,{
+      //         y:0,
+      //         duration: 0.4
+      //     },0.1)
+      //     .to(m.uniforms.uCorners.value,{
+      //         z:0,
+      //         duration: 0.4
+      //     },0.2)
+      //     .to(m.uniforms.uCorners.value,{
+      //         w:0,
+      //         duration: 0.4
+      //     },0.3)
+      // })
+
+      let mesh = new THREE.Mesh(this.geometry, m);
+      this.scene.add(mesh);
+      mesh.scale.set(bounds.width, bounds.height, 1);
+      return {
+        img: img,
+        mesh: mesh,
+        width: bounds.width,
+        height: bounds.height,
+        top: bounds.top,
+        left: bounds.left,
+      };
+    });
+
+}
+
+setPosition() {
+// console.log(this.asscroll.currentPos)
+if (!this.animationRunning) {
+this.imageStore.forEach((o) => {
+o.mesh.position.x =
+-this.asscroll.currentPos + o.left - this.width / 2 + o.width / 2;
+o.mesh.position.y = -o.top + this.height / 2 - o.height / 2;
+});
+}
+}
+
+render() {
+this.time += 0.05;
+this.material.uniforms.time.value = this.time;
+
+    this.asscroll.update();
+    this.setPosition();
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.render.bind(this));
+
+}
+}
+
+new Sketch({
+domElement: document.getElementById("container"),
+});
